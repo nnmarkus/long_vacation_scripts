9bcf580c65c5763f5c130525941e6a85a024e3b4
Nathan Markus <nathan@natem.win>
2024-06-04 08:26:14 -0500

    Moved text to readme file.

--- SetLongVacationV2.ps1
+++ SetLongVacationV2.ps1
@@ -1,26 +1,8 @@
-﻿<# 
-POTENTIAL TO DO LIST:
-1.  Add SMTP notifications for when the set script is ran and when the end/start scripts are ran. 
-    Maybe a new distro like "longvacationnotifications@"
-
-
-
-This script can be used to create scheduled tasks (preferably on a domain controller) to 
-disable and re-enable users when they are known to be away for a extended period of time. 
-!!! this runs with the following settings so it MUST be set on a domain controller. 
-1. "Run whether user is logged on or not"
-2. Run as SYSTEM
-3. Run with highest privileges
-
-
-
-#>
+﻿Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
 
 $rootDir="C:\scripts\long_vacation_scripts\"
 
-
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
 
 function Show-DateTimePickerForm {
     param (
